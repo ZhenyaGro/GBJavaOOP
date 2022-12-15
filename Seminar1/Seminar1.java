d1060d1ae08768b75748677d61a6d45760c1e606
ZhenyaGro <bon4ios@gmail.com>
2022-12-15 23:46:09 +0300

    Testing of classes

--- Seminar1/Seminar1.java
+++ Seminar1/Seminar1.java
@@ -2,6 +2,14 @@ package Seminar1;
 
 public class Seminar1 {
   public static void main(String[] args) {
+    FamilyTree familyTree = new FamilyTree();
 
+    Human ivanIvanov = new Human("Ivan Ivanov", "Male");
+    Human mariaIvanova = new Human("Maria Ivanova", "Female");
+
+    Human petrIvanov = new Human("Petr Ivanov", "Male", mariaIvanova, ivanIvanov);
+
+    // System.out.println(petrIvanov);
+    System.out.println(petrIvanov.getInfo());
   }
 }
