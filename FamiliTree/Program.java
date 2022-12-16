06aceaa76f9d2a2befd9df001b0ae82debfe720a
ZhenyaGro <bon4ios@gmail.com>
2022-12-16 22:22:27 +0300

    Added menu

---
+++ FamiliTree/Program.java
@@ -0,0 +1,57 @@
+package FamiliTree;
+
+import java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+    FamilyTree familyTree = new FamilyTree();
+
+    Human ivanIvanov = new Human("Иван Иванов", "Мужской");
+    Human mariaIvanova = new Human("Мария Иванова", "Женский");
+    Human petrIvanov = new Human("Петр Иванов", "Мужсой", mariaIvanova, ivanIvanov);
+
+    familyTree.addHuman(ivanIvanov);
+    familyTree.addHuman(mariaIvanova);
+    familyTree.addHuman(petrIvanov);
+
+    Scanner str = new Scanner(System.in);
+
+    System.out.print(
+        "Меню:\n1 - Показать всех людей\n2 - Найти человека по имени и фамилии\n3 - Добавить нового человека\nВыбрано: ");
+
+    String selectedItem = str.next();
+
+    String fullName;
+    switch (selectedItem) {
+      case "1":
+        System.out.println("\nЛюди из семейного дерева:");
+        familyTree.showHumans();
+        break;
+
+      case "2":
+        System.out.println("\nВведите имя и фамилию: ");
+        fullName = str.next() + " " + str.next();
+        Human foundPerson = familyTree.searchByName(fullName);
+        if (foundPerson != null) {
+          System.out.println("Получить дополнительную информацию о найденном человеке? (y/n): ");
+          if (str.next().toLowerCase().equals("y"))
+            System.out.println(foundPerson.getInfo());
+        } else {
+          System.out.println("Человек не найден");
+        }
+        break;
+
+      case "3":
+        // System.out.println("\nВведите имя и фамилию: ");
+        // fullName = str.next() + " " + str.next();
+        // String gender = str.next();
+        // familyTree.addHuman(fullName, gender);
+        break;
+
+      default:
+        System.out.println("Выбран недействительный пункт меню");
+        break;
+    }
+
+  }
+}
